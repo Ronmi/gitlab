a953b229ee5487b83e94e09600646c6a24226861
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-31 09:35:13 +0800

    api to create and delete branch

--- branch_api.go
+++ branch_api.go
@@ -1,6 +1,9 @@
 package gitlab
 
-import "strconv"
+import (
+	"net/url"
+	"strconv"
+)
 
 // ListBranches retrieves all branches in a project
 //
@@ -9,5 +12,35 @@ func (g *GitLab) ListBranches(pid int, opts *ListOption) (ret []Branch, page *Pa
 	uri := g.uri("/projects/:id/repository/branches", map[string]string{":id": strconv.Itoa(pid)})
 	resp, page, err := g.get(uri, opts)
 	err = forgeRet(resp, &ret, err)
+	return
+}
+
+// CreateBranch creates new branch in a project
+//
+// See http://docs.gitlab.com/ce/api/branches.html#create-repository-branch
+func (g *GitLab) CreateBranch(pid int, name, ref string) (ret Branch, err error) {
+	if ref == "" {
+		ref = "master"
+	}
+
+	uri := g.uri("/projects/:id/repository/branches", map[string]string{":id": strconv.Itoa(pid)})
+
+	opts := url.Values{}
+	opts.Set("branch_name", name)
+	opts.Set("ref", ref)
+
+	resp, _, err := g.postForm(uri, opts)
+	err = forgeRet(resp, &ret, err)
+	return
+}
+
+// DeleteBranch deletes a branch from project
+//
+// See http://docs.gitlab.com/ce/api/branches.html#delete-repository-branch
+func (g *GitLab) DeleteBranch(pid int, name string) (err error) {
+	uri := g.uri("/projects/:id/repository/branches/:br", map[string]string{":id": strconv.Itoa(pid), ":br": name})
+
+	resp, _, err := g.delete(uri, nil)
+	err = forgeRet(resp, nil, err)
 	return
 }
--- branch_api_test.go
+++ branch_api_test.go
@@ -1,6 +1,7 @@
 package gitlab
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -15,26 +16,86 @@ func findBranch(brs []Branch, name string) *Branch {
 	return nil
 }
 
-func TestListBranches(t *testing.T) {
+func TestBranch(t *testing.T) {
 	c := makeClient()
+	if err := BranchTestList(c); err != "" {
+		t.Fatal(err)
+	}
+
+	if err := BranchTestCreate(c, "orz"); err != "" {
+		t.Fatal(err)
+	}
+	if err := BranchTestDelete(c, "orz"); err != "" {
+		t.Fatal(err)
+	}
+}
+
+func BranchTestList(c *GitLab) string {
 	brs, page, err := c.ListBranches(RepoID, nil)
 	if err != nil {
-		t.Fatalf("Unexpected error when calling GET /projects/:id/branches: %s", err)
+		return fmt.Sprintf("Unexpected error when calling GET /projects/:id/branches: %s", err)
 	}
 
 	expectPage := Pagination{}
 	if p := *page; !reflect.DeepEqual(expectPage, p) {
-		t.Errorf("Pagination info should be zero value, got %#v", p)
+		return fmt.Sprintf("Pagination info should be zero value, got %#v", p)
 	}
 
 	// There should be only 2 branches, one is master and another is test_branch
 	if l := len(brs); l != 2 {
-		t.Errorf("Expected to have 2 branches, got %d", l)
+		return fmt.Sprintf("Expected to have 2 branches, got %d", l)
 	}
 	if b := findBranch(brs, "master"); b == nil {
-		t.Errorf("Branch master not found")
+		return fmt.Sprintf("Branch master not found")
 	}
 	if b := findBranch(brs, "test_branch"); b == nil {
-		t.Errorf("Branch test_branch not found")
+		return fmt.Sprintf("Branch test_branch not found")
 	}
+
+	return ""
+}
+
+func BranchTestCreate(c *GitLab, name string) string {
+	br, err := c.CreateBranch(RepoID, name, "") // based on master
+	if err != nil {
+		return fmt.Sprintf("Unexpected error occured when creating branch: %s", err)
+	}
+	if br.Name != name {
+		return fmt.Sprintf("Expected to create branch %s, but %s created instead", name, br.Name)
+	}
+
+	brs, _, err := c.ListBranches(RepoID, nil)
+	if err != nil {
+		return fmt.Sprintf("Unexpected error when fetching branches: %s", err)
+	}
+
+	if l := len(brs); l != 3 {
+		return fmt.Sprintf("Expected to have 3 branches, got %d", l)
+	}
+	if b := findBranch(brs, name); b == nil {
+		return fmt.Sprintf("Expected to have %s branch, but not found", name)
+	}
+
+	return ""
+}
+
+func BranchTestDelete(c *GitLab, name string) string {
+	err := c.DeleteBranch(RepoID, name)
+	if err != nil {
+		return fmt.Sprintf("Unexpected error occured when deleteing branch: %s", err)
+	}
+
+	brs, _, err := c.ListBranches(RepoID, nil)
+	if err != nil {
+		return fmt.Sprintf("Unexpected error when fetching branches: %s", err)
+	}
+
+	if l := len(brs); l != 2 {
+		return fmt.Sprintf("Expected to have 2 branches, got %d", l)
+	}
+	if b := findBranch(brs, name); b != nil {
+		return fmt.Sprintf("Expected not to have %s branch, but found", name)
+	}
+
+	return ""
 }
